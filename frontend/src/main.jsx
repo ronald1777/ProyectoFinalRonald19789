5a7bccc77870f927215839bf361202c73bd221fe
Ronald <romanuelcatun13@gmail.com>
2026-05-26 18:47:09 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from './context/ThemeContext'
+import { StorageProvider } from './context/StorageContext'
+import { UserProvider } from './context/UserContext'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <UserProvider>
+        <StorageProvider>
+          <App />
+        </StorageProvider>
+      </UserProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
